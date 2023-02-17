2e871d9540346bcd7b7d487a9f59de11041c29ee
Ian Thuku <thuku0249@gmail.com>
2023-02-17 08:06:54 -0800

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,5 @@
 
 #include <stdio.h>
-
 /**
  * main - prints alpha in lower case
  * Return: Always 0
@@ -8,10 +7,8 @@
 int main(void)
 {
 char lc;
-
 for (lc = a; lc <= z; lc++)
-{
-putchar(lc);
+{putchar(lc);
 }
 putchar(n);
 return (0);
