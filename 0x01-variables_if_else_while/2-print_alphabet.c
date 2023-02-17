fab2c4635e3a2f6859c013eaa9f0a158ddf64451
Ian Thuku <thuku0249@gmail.com>
2023-02-17 08:21:51 -0800

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,20 @@
 
 #include <stdio.h>
+
 /**
- * main - prints alpha in lower case
- * Return: Always 0
+ * main - prints the alphabet
+ *
+ * Return; Always 0 (Success)
  */
 int main(void)
 {
-char lc;
-for (lc = a; lc <= z; lc++)
-{putchar(lc);
-}
-putchar(n);
-return (0);
+ char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+ int i;
+
+ for (i = 0; i < 26; i++)
+ {
+  putchar(alp[i]);
+ }
+ putchar(n);
+ return (0);
 }
